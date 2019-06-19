064cc928a7f3d0f9473e2e5d12bc36848f5fafc1
MarcioLucas22 <48500194+MarcioLucas22@users.noreply.github.com>
2019-06-19 20:21:52 -0300

    Update comentario.js

--- comentario.js
+++ comentario.js
@@ -1,23 +1,23 @@
 function post() {
-	
+	//collect user input
 	var username = document.getElementById("username").value
 	
 	var comment = document.getElementById("comment").value
 	var emptyDiv = document.getElementById("emptyDiv").value
-
+	//creating new tags
 	var d = new Date()
 	var p = document.createElement("p")
 	
 	var h1 = document.createElement("p")
 	var h3 = document.createElement("p")
 	
-	
+	//putting information into the new tags
 	p.innerHTML = d.toString()
 	
 	h1.innerHTML = username
 	h3.innerHTML = comment
 	
-	
+	//putting the new tags into the emptyDiv on page
 	document.getElementById("emptyDiv").appendChild(p)
 	
 	document.getElementById("emptyDiv").appendChild(h1)
